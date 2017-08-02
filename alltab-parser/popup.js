b21229d71cda26eb3ca3a9cf229ecc2879f6928d
Natpaphon Sukitpaneenit <natpasukit@gmail.com>
2017-08-02 12:42:20 +0700

    Seperate gettab into function

--- alltab-parser/popup.js
+++ alltab-parser/popup.js
@@ -1,15 +1,20 @@
+// Get URL from tab function
+function gettab(){
+  chrome.windows.getAll({populate:true},function(windows){
+    windows.forEach(function(window){
+      window.tabs.forEach(function(tab){
+        //collect all of the urls here, I will just log them instead
+        console.log(tab.url);
+      });
+    });
+  });
+}
+
 // Add onClick listener to checkTab button
 document.addEventListener('DOMContentLoaded',function(){
   var checkPageButton = document.getElementById('checkTab');
   checkPageButton.addEventListener('click',function(){
-    //Get all open tab
-    chrome.windows.getAll({populate:true},function(windows){
-      windows.forEach(function(window){
-        window.tabs.forEach(function(tab){
-          //collect all of the urls here, I will just log them instead
-          console.log(tab.url);
-        });
-      });
-    });
+    //Get all open tab to console.log
+    gettab();
   });
 });
