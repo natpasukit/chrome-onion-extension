a035b666bf694cb32432c9e467029771deffbb41
Natpaphon Sukitpaneenit <natpasukit@gmail.com>
2017-08-02 14:36:25 +0700

    add urltitle
    alltab-parser
    - add key for array
    - add tab title
    - check localstorage for browser for further use ?

--- alltab-parser/popup.js
+++ alltab-parser/popup.js
@@ -1,13 +1,18 @@
 // Get URL from tab function
 function gettab(){
+  var url = [];
   chrome.windows.getAll({populate:true},function(windows){
     windows.forEach(function(window){
       window.tabs.forEach(function(tab){
-        //collect all of the urls here, I will just log them instead
-        console.log(tab.url);
+        //Push all tab into array
+        url.push( {
+          "url":tab.url,
+          "title":tab.title
+        } );
       });
     });
   });
+  return(url);
 }
 
 // Add onClick listener to checkTab button
@@ -15,6 +20,12 @@ document.addEventListener('DOMContentLoaded',function(){
   var checkPageButton = document.getElementById('checkTab');
   checkPageButton.addEventListener('click',function(){
     //Get all open tab to console.log
-    gettab();
+    console.log(gettab());
+    //Check browserver localstorage
+    if(window.localStorage) {
+      console.log('localstorage is available');
+    } else {
+      console.log('localstorage is unavailable');
+    }
   });
 });
