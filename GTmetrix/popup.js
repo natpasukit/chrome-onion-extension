9cd8f428c60dce51870a15e9580660bcf7ec5ff1
Natpaphon Sukitpaneenit <natpasukit@gmail.com>
2017-08-02 12:35:30 +0700

    Add tab parser extension
    Parser Extension
    - parse all tab url into console log.
    GTmetrix
    - add tab permission in manifrst

--- GTmetrix/popup.js
+++ GTmetrix/popup.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
-
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
-
       var f = d.createElement('form');
       f.action = 'http://gtmetrix.com/analyze.html?bm';
       f.method = 'post';
---
+++ alltab-parser/popup.js
@@ -0,0 +1,15 @@
+// Add onClick listener to checkTab button
+document.addEventListener('DOMContentLoaded',function(){
+  var checkPageButton = document.getElementById('checkTab');
+  checkPageButton.addEventListener('click',function(){
+    //Get all open tab
+    chrome.windows.getAll({populate:true},function(windows){
+      windows.forEach(function(window){
+        window.tabs.forEach(function(tab){
+          //collect all of the urls here, I will just log them instead
+          console.log(tab.url);
+        });
+      });
+    });
+  });
+});
