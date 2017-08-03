e5942c5ab780eb116f942e5718aa23f615e9d3eb
Natpaphon Sukitpaneenit <natpasukit@gmail.com>
2017-08-04 00:30:10 +0700

    Add loadpage button
    Finish 1.1 !
    You can now load and save tab for later

--- alltab-parser/popup.js
+++ alltab-parser/popup.js
@@ -20,6 +20,11 @@ document.addEventListener('DOMContentLoaded',function(){
       });
         setTimeout(function(){
             var JSONurl = JSON.stringify(url);
+            // If localstorage key exist clear else set it
+            if (localStorage.getItem("TabLog") === null) {
+              // Remove old Data
+              localStorage.removeItem('TabLog');
+            }
             localStorage.setItem('TabLog',JSONurl);
             var retrievedObject = localStorage.getItem('TabLog');
             console.log('retrievedObject: ', JSON.parse(retrievedObject));
@@ -28,5 +33,16 @@ document.addEventListener('DOMContentLoaded',function(){
       alert('Your browser does not support localstorage');
       console.log('Your browser does not support localstorage');
     }
+  });
+
+  var loadPageButton = document.getElementById('loadTab');
+  loadPageButton.addEventListener('click',function(){
+    //Load data into app
+    var retrievedObject = JSON.parse(localStorage.getItem('TabLog'));
+    console.log(retrievedObject);
+    retrievedObject.forEach(function(current_url){
+      console.log(current_url.url);
+      chrome.tabs.create({ url: current_url.url });
+    });
   });
 });
