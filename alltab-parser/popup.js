015c85876286c0ba074122e4ae7d0213913c5f15
Natpaphon Sukitpaneenit <natpasukit@gmail.com>
2017-08-03 14:08:54 +0700

    alttab-parser
    - remove localstorage
    - add indexedDB

--- alltab-parser/popup.js
+++ alltab-parser/popup.js
@@ -1,13 +1,13 @@
 // Get URL from tab function
 function gettab(){
-  var url = [];
+  url = [];
   chrome.windows.getAll({populate:true},function(windows){
     windows.forEach(function(window){
       window.tabs.forEach(function(tab){
         //Push all tab into array
         url.push( {
-          "url":tab.url,
-          "title":tab.title
+          "url":tab.url
+          ,"title":tab.title
         } );
       });
     });
@@ -21,11 +21,28 @@ document.addEventListener('DOMContentLoaded',function(){
   checkPageButton.addEventListener('click',function(){
     //Get all open tab to console.log
     console.log(gettab());
-    //Check browserver localstorage
-    if(window.localStorage) {
-      console.log('localstorage is available');
-    } else {
-      console.log('localstorage is unavailable');
+    // Using indexedDB
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+    window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+    // Check support for indexedDB
+    if (!window.indexedDB) {
+       window.alert("Your browser doesn't support a stable version of IndexedDB.")
     }
+    var db;
+    var request = indexedDB.open("TAB Log");
+    request.onerror = function(event) {
+      alert("Please allow IndexedDB!");
+    };
+    request.onsuccess = function(event) {
+      db = event.target.result;
+    };
   });
 });
+
+// Global error handle for indexedDB
+// db.onerror = function(event) {
+//   // Generic error handler for all errors targeted at this database's
+//   // requests!
+//   alert("Database error: " + event.target.errorCode);
+// };
